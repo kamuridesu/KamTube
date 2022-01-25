22903c1da514a98a590d17b7a20b0cddf0c422f6
kamuridesu <myk.gata14@gmail.com>
2022-01-25 08:52:45 -0300

    finally some good fcking food

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ import axios from 'axios';
 import { parse } from 'node-html-parser';
 import fs from 'fs';
 
-class YTBTromloader {
-    constructor(url) {
-        this.url = url
+class KamTube {
+    constructor() {
+        this.base_api_url = "https://invidious.namazso.eu/api/v1/";
     }
 
-    async fetcher() {
-        const response = await axios.get(this.url);
+    async fetcher(url) {
+        const response = await axios.get(url);
         return response.data;
     }
 
-    async getVideoInfos() {
-        let data = await this.fetcher();
+    async search(query, page, sort_by, date, duration, type, region) {
+        page = page ? page : 1
+        sort_by = sort_by ? sort_by : "relevance"
+        date = date ? "&date=" + date : ""
+        duration = duration ? "&duration" : ""
+        type = type ? type : "video"
+        region = region ? region : "US"
+        const full_query = `search?q=${query}&page=${page}&sort_by=${sort_by}&${date}&${duration}&type=${type}&region=${region}`
+        return await this.fetcher(this.base_api_url + full_query)
+    }
+
+    async getFullMetadata(video_id) {
+        return await this.fetcher(this.base_api_url + `videos/${video_id}`)
+    }
+
+    async getVideoInfos(video_id) {
+        let data = await this.fetcher("https://ytb.trom.tf/watch?v=" + video_id);
         let parser = parse(data);
         let infos = [];
         let options = parser.getElementsByTagName("select")[0].childNodes;
@@ -31,22 +46,30 @@ class YTBTromloader {
         return infos;
     }
 
-    async getVideoDownloadUrl(id) {
-        let base_url = "https://ytb.trom.tf/latest_version?download_widget=";
-        let data = await this.getVideoInfos();
+    async getVideoDownloadUrl(video_id, quality) {
+        quality = quality ? quality : "360"
+        const base_url = "https://ytb.trom.tf/latest_version?download_widget=";
+        const data = await this.getVideoInfos(video_id);
         try{
-            let request_url = base_url + encodeURI((data[id].infos));
+            let request_url = base_url
+            for(let d of data) {
+                if(d.name.includes(quality)) {
+                    request_url += encodeURI(d.infos);
+                    break;
+                }
+            }
             return request_url;
         } catch (e) {
             console.log(e);
+            return null;
         }
     }
 
-    async download(id) {
+    async download(quality) {
         try {
             const response = await axios({
                 method: "get",
-                url: await this.getVideoDownloadUrl(id),
+                url: await this.getVideoDownloadUrl(quality),
                 headers: {
                     "DNT": 1,
                     "Upgrade-Insecure-Request": 1
@@ -60,6 +83,17 @@ class YTBTromloader {
         }
     }
 
+    async getThumbnail(video_id) {
+        let data = await this.getFullMetadata(video_id);
+        let quality = "maxres";
+        for (let d of data.videoThumbnails) {
+            if (d.quality === quality) {
+                console.log(d)
+                return await this.fetcher(d.url);
+            }
+        }
+    }
+
     async save(id, path) {
         let data = await this.download(id);
         if (data) {
@@ -69,4 +103,4 @@ class YTBTromloader {
 }
 
 
-export default YTBTromloader;
+export default KamTube;
