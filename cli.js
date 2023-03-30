a26ddcc6beb460f510b51da2f9c18cabf5c8ce35
kamuridesu <myk.gata14@gmail.com>
2023-03-30 00:26:53 -0300

    Major refactor based of KamTube.py

--- cli.js
+++ cli.js
@@ -1,119 +1,150 @@
-#!/usr/bin/env node
-import KamTube from './index.js';
-import _yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import { KamTube } from './index.js';
+import { formatDuration } from "./src/parsers.js";
+
 import * as readline from 'readline';
-const rline = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-const yargs = _yargs(hideBin(process.argv))
-const kamtube = new KamTube("cli");
 
-class cli {
-    constructor(extract) {
-        this.audio_only = extract;
-        this.choosen_video = '';
-        this.quality = undefined;
-    }
+class CLI {
+  constructor(media_type = 'mixed') {
+    this.media_type = media_type;
+    this.downloader = new KamTube('cli');
+  }
 
-    async direct(args) {
-        let option = 0;
-        if (this.audio_only) option = 1;
-        await kamtube.save((await kamtube.search(args))[0].videoId, option);
-    }
+  async search(query) {
+    const results = await this.downloader.search(query);
+    const template = '[{id}] - {title} - {duration}';
+    const results_formatted = results
+      .map((result, id) =>
+        template.replace(
+          '{id}', id
+        ).replace(
+          '{title}', result.title
+        ).replace(
+          '{duration}', formatDuration(result.lengthSeconds)
+        )
+      ).join('\n');
+    return {
+      text: results_formatted,
+      results
+    };
+  }
 
-    async download(args) {
-        let option = 0;
-        if (this.audio_only) option = 1;
-        await this.search(args);
-        rline.on('close', async () => {
-            // console.log(this.choosen);
-            await kamtube.save(this.choosen_video.videoId, option, this.quality);
-            process.exit(0);
-        });
-    }
+  async interactiveSearch(query) {
+    const results = await this.search(query);
+    console.log(results.text);
 
-    async search(args, output) {
-        let results = await kamtube.search(args);
-        for(const [index, res] of results.entries()) {
-            let out = `[${index}] - ${res.title} - ${res.author} - ${res.lengthSeconds}s`
-            console.log(out);
+    const rline = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+  
+    return new Promise((resolve, reject) => {
+      rline.question('Choose an option >>> ', async (option) => {
+        try {
+          option = parseInt(option);
+          if (option < 0 || option > results.results.length - 1) {
+            throw new Error();
+          }
+          const quality = await this.qualitySelector(results.results[option]);
+          resolve(await this.save(results.results[option], quality));
+        } catch (error) {
+          console.log(`Option needs to be a number between 0 and ${results.results.length - 1}`);
+          reject(error);
+        } finally {
+          rline.close();
         }
-        console.log("\n");
-        if(output) {
-            process.exit(0);
+      });
+    });
+  }
+  
+
+  async qualitySelector(result) {
+    const qualities = (await this.downloader.getMediaInfo(result.videoId))[this.media_type];
+    const qualities_str = this.media_type === 'video' ?
+      qualities.map((quality, index) =>
+        `[${index}] - Resolution: ${quality.quality};FPS: ${quality.fps}`
+      ).join('\n') :
+      qualities.map((quality, index) =>
+        `[${index}] - Quality: ${quality.quality}`
+      ).join('\n');
+    console.log(qualities_str);
+    
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  
+    return new Promise((resolve, reject) => {
+      rl.question('Choose a quality >>> ', (option) => {
+        rl.close();
+        if (option < 0 || option > qualities.length - 1 || isNaN(option)) {
+          reject(new Error(`Option needs to be a number between 0 and ${qualities.length - 1}`));
+        } else {
+          resolve(qualities[option].quality);
         }
-        let option = 'Error!';
-        rline.question('Choose an option >>> ', async (opt) => {
-            option = opt;
-            if (option > results.length - 1) {
-                console.log("Error! Cannot access the result!");
-                process.exit(1);
-            }
-            const choosen_video = results[option];
-            // console.log(choosen_video);
-            this.choosen_video = choosen_video;
-            let aud_only = 0;
-            if (this.audio_only) aud_only = 1;
-            const qualities = (await kamtube.getMediaQuality(choosen_video.videoId, aud_only)).qualities;
-            console.log("Now choose the quality: ");
-            for(const [index, qua] of qualities.entries()) {
-                const out = `[${index}] - ${qua.name}`
-                console.log(out);
-            }
-            console.log("\n");
-            let choose = 'Error';
-            rline.question("Choose a quality >>> ", async (o) => {
-                choose = o;
-                if(choose > qualities.length - 1) {
-                    console.log("Error! Cannot access the result!");
-                    process.exit(1);
-                }
-                const choosen_quality = qualities[choose];
-                this.quality = choosen_quality;
-                rline.close();
-            });
-        });
-    }
+      });
+    });
+  }
+
+  async directDownload(query) {
+    const results = await this.search(query);
+    return await this.save(results.results[0]);
+  }
+
+  async save(result, quality = null) {
+    await this.downloader.save(result.videoId, this.media_type, quality);
+  }
 }
 
-// get the arguments, -x or --extract extracts the audio from the video
-function main () {
-    const argv = yargs.options({
-        'extract': {
-            alias: 'x',
-            type: 'boolean',
-            default: false,
-            description: 'Extract the audio from the video'
-        },
-        'help': {
-            alias: 'h',
-            type: 'boolean',
-            description: 'Show the help'
-        },
-        'search': {
-            alias: 's',
-            type: 'boolean',
-            description: 'Search for a video or song'
-        },
-        'direct': {
-            alias: 'd',
-            type: 'boolean',
-            description: 'Directly downloads the video or audio without search prompt'
-        }
-    }).argv;
-    const _cli = new cli(argv.extract);
-    if (argv._.length < 1) {
+async function argparse() {
+  const args = process.argv.slice(2);
+  const usage = () => {
+    console.log(`Usage: kamtube [flags] search query
+Flags:
+  -h or --help: Print this message
+  -d or --direct: Direct download (non interactive)
+  -x or --extract: download audio only`);
+  };
+  if (!args.length) {
+    usage();
+    throw new Error();
+  }
+  const known_flags = ['-x', '--extract', '-d', '--direct', '-h', '--help'];
+  let extract = 'mixed';
+  let direct = false;
+  const query_args = [];
+  for (const arg of args) {
+    switch (arg) {
+      case '-x':
+      case '--extract':
+        extract = 'audio';
+        break;
+      case '-d':
+      case '--direct':
+        direct = true;
+        break;
+      case '-h':
+      case '--help':
+        usage()
         return;
+      default:
+        if (known_flags.includes(arg)) {
+          console.log(`Unknown flag: ${arg}`);
+          usage();
+          throw new Error();
+        }
+        query_args.push(arg);
     }
-    if (argv.search) {
-        return _cli.search(argv._.join(" "), true);
-    }
-    if (argv.direct) {
-        return _cli.direct(argv._.join(" "));
-    }
-    return _cli.download(argv._.join(" "));
+  }
+  const cli = new CLI(extract);
+  const query = query_args.join(' ');
+  if (direct) {
+    await cli.directDownload(query);
+    console.log('Download complete!');
+  } else {
+    await cli.interactiveSearch(query);
+    console.log('Download complete!');
+  }
 }
 
-main();
+argparse();
+
--- index.js
+++ index.js
@@ -1,280 +1,161 @@
-import axios from 'axios';
-import { parse } from 'node-html-parser';
-import fs from 'fs';
-
-
-/*
-*   @param {string} url
-*   @return {string} with the url of the video
-*   @description: Get the video id from the url
-*/
-async function urlParser(url) {
-    url = url.includes("https://") ? url.split("https://")[1] : (url.includes("https://") ? url.split("http://")[1] : url);
-    if (url.includes("shorts")) {
-        url = url.split("shorts/")[1];
-    }
-    if (url.includes("watch")) {
-        url = url.split("watch?v=")[1]
-    }
-    if (url.includes("youtu.be")) {
-        url = url.split("youtu.be/")[1];
-    }
-    if (url.includes("?")) {
-        url = url.split("?")[0];
-    }
-    return url;
-}
-
-/*
-*   @param {string} url: The url of the video
-*   @param {number} count: Retry count
-*   @return {object} with the data
-*   @description: Get the content of the url
-*/
-async function fetcher(url, count) {
-    count = count ? count : 0;
-    url = encodeURI(url);
-    if (count >= 3) {
-        if (this.cli) {
-            console.log("Too many requests, aborting");
-            process.exit(1);
-        }
-        throw new Error("Max retries");
-    }
-    try {
-      const response = await axios.get(url);
-      return response.data;
-    } catch (e) {
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        fetcher(url, count + 1);
-    }
-}
-
+import { get, post } from "./src/networking.js"
+import { urlParse, parseVideoPage } from "./src/parsers.js";
+import { promises as fs } from 'fs';
 
 class KamTube {
-    constructor(mode, debug) {
-        this.cli = false;
-        this.debug = debug ? debug: false;
-        if (mode == "cli") this.cli = true;
+    constructor(mode="string", debug=false) {
+        this.mode = mode;
+        this.debug = debug;
+        this.cli = mode === "cli";
         this.base_api_url = "https://invidious.namazso.eu/api/v1/";
+        this.video_html_page = "https://ytb.trom.tf/watch?v=";
         this.download_endpoint = "https://ytb.trom.tf/download";
     }
 
-    cli_log(msg) {
-        if (this.cli) console.log(msg);
+    async cliLog(message) {
+        if (this.cli) {
+            console.log(message);
+        }
     }
 
-    debug_log(msg) {
-        if (this.debug) console.log(msg);
+    async search(
+        query,
+        page="",
+        date="",
+        sort_by="",
+        duration="",
+        _type="",
+        region=""
+    ) {
+        date = date ? `&date=${date}` : "";
+        duration = duration != 0 ? `duration=${duration}` : "";
+        region = region ? `&region=${region}` : "";
+        const full_query = `search?q=${query}&page=${page}&sort_by=${sort_by}${duration}${date}&type=${_type}${region}`;
+        const response = (await get(this.base_api_url + full_query));
+        return response;
     }
 
-    /*
-    *   @param {string} query
-    *   @param {string} page
-    *   @param {string} sort_by
-    *   @param {string} date
-    *   @param {string} duration
-    *   @param {string} type
-    *   @param {string} region
-    *   @return {Array}
-    *   @description: Searches for a video
-    */
-    async search(query, page, sort_by, date, duration, type, region) {
-        this.cli_log("Searching for: " + query);
-        page = page ? page : 1
-        sort_by = sort_by ? sort_by : "relevance"
-        date = date ? "&date=" + date : ""
-        duration = duration ? "&duration" + duration : ""
-        type = type ? type : "video"
-        region = region ? region : "US"
-        const full_query = `search?q=${query}&page=${page}&sort_by=${sort_by}${date}${duration}&type=${type}&region=${region}`
-        return await fetcher(this.base_api_url + full_query)
-    }
-
-    /*
-    *   @param {string} video_id
-    *   @return {object}
-    *   @description: Get the video full info
-    */
-    async getFullMetadata(video_id) {
-        video_id = await urlParser(video_id);
-        return await fetcher(this.base_api_url + `videos/${video_id}`)
-    }
-
-    /*
-    *   @param {string} video_id
-    *   @return {Array}
-    *   @description: Get the video info to build the query
-    */
-    async getVideoInfos(video_id) {
-        video_id = await urlParser(video_id);
-        let data = await fetcher("https://ytb.trom.tf/watch?v=" + video_id);
-        let parser = parse(data);
-        let options = parser.getElementsByTagName("select")[0].childNodes;
-        const name = parser.getElementsByTagName("title")[0].innerText.replace(" - Invidious", "").trim();
-        let infos = {name: name, mixed: [], audio: [], video: []};
-        for (let i = 0; i < options.length; i++) {
-            let option = options[i];
+    async getMediaMetadata(media_id){
+        media_id = urlParse(media_id);
+        const result = (
+            await get( this.base_api_url + `videos/${media_id}`)
+        )
+        if (!result) {
             try{
-                let attrs = option.attrs;
-                if (attrs != undefined) {
-                    let quality_name = option.text.trim();
-                    if (quality_name.includes("audio only")) {
-                        infos.audio.push({
-                            name: quality_name,
-                            url: attrs.value
-                        });
-                    } else if (quality_name.includes("video only")) { 
-                        infos.video.push({
-                            name: quality_name,
-                            url: attrs.value
-                        });
-                    } else {
-                        infos.mixed.push({
-                            name: quality_name,
-                            url: attrs.value
-                        });
-                    }
-                }
-            } catch (e) {
-                //
+                const json_error = result.error;
+                console.error(
+                    "Error while retrieving "
+                    + this.base_api_url
+                    + `videos/${media_id}: `
+                    + json_error
+                );
+                throw Error(json_error);
+            } catch (e){
+                throw Error(e);
             }
         }
-        return infos;
+        return result;
     }
 
-    /* @param {string} media_id
-    *  @param {number} audio_or_video 1 for audio only, 2 for video only
-    *  @return {object}
-    *  @description: Get the media resolution options
-    */
-    async getMediaQuality(media_id, audio_or_video) {
-        this.cli_log("Getting quality options...");
-        media_id = await urlParser(media_id);
-        const base_url = "https://ytb.trom.tf/latest_version?download_widget=";
-        const media_full_data = await this.getVideoInfos(media_id);
-        let media_data = media_full_data.mixed;
-        if(audio_or_video == 1) {
-            media_data = media_full_data.audio;
-        } else if(audio_or_video == 2) {
-            media_data = media_full_data.video;
+    async getMediaInfo(media_id){
+        media_id = urlParse(media_id);
+        const headers = {
+            "Host": "ytb.trom.tf",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "DNT": 1,
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": 1,
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Sec-GPC": 1,
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache"
         }
-        return {'media_title': media_full_data.name, "qualities": media_data};
+        const data = (await get(this.video_html_page + media_id, headers, "text"));
+        return parseVideoPage(data);
     }
 
-    /*
-    *   @param {string} media_id
-    *   @param {number} audio_or_video
-    *   @param {string} quality
-    *   @return {object}
-    *   @description: Get the video url, pass 0 to get the full video, 1 to get the audio only, 2 to get the video only
-    */
-    async getMediaDownloadBody(media_id, audio_or_video, quality) {
-        this.cli_log("Getting video url");
-        media_id = await urlParser(media_id);
-        let aviable_qualities_infos = await this.getMediaQuality(media_id, audio_or_video);
-        let aviable_qualities = aviable_qualities_infos.qualities;
-        let media_title = aviable_qualities_infos.media_title;
-        if(quality) {
-            if(quality == 'max') {
-                quality = audio_or_video == 0 ? aviable_qualities[0] : aviable_qualities[aviable_qualities.length - 1];
-            } else {
-                for(let i = 0; i < aviable_qualities.length; i++) {
-                    if(aviable_qualities[i].name.includes(quality)) {
-                        quality = aviable_qualities[i];
-                    }
-                }
-            }
-        } else {
-            quality = aviable_qualities.length > 1 ? aviable_qualities[1] : aviable_qualities[0];
-        }
-        let body = {
-            'id': media_id,
-            'title': media_title,
-            'download_widget': quality.url
-        }
-        return body;
-    }
-
-    /*
-    *   @param {string} video_id
-    *   @param {string} quality
-    *   @param {number} audio_or_video
-    *   @return {object}
-    *   @description: Get the video
-    */
-    async download(media_id, audio_or_video, quality) {
-        media_id = await urlParser(media_id);
-        const media_download_body = await this.getMediaDownloadBody(media_id, audio_or_video, quality);
-        const body = `id=${media_download_body.id}&title=${media_download_body.title}&download_widget=${encodeURIComponent(media_download_body.download_widget)}`
-        const title = media_download_body.title;
-        try {
-            this.cli_log("Downloading media");
-            const response = await axios({
-                method: "post",
-                url: this.download_endpoint,
-                data: body,
-                headers: {
-                    "DNT": 1,
-                    "Upgrade-Insecure-Request": 1
-                },
-                responseType:'arraybuffer'
-            });
-            return {title: title, data: response.data}
-        } catch (e) {
-            console.log(e);
-            return null;
-        }
-    }
-
-    /*
-    *   @param {string} video_id
-    *   @return {string}
-    *   @description: Get the video thumbnail
-    */
-    async getThumbnail(video_id) {
-        video_id = await urlParser(video_id);
-        let data = await this.getFullMetadata(video_id);
-        let quality = "maxres";
-        for (let d of data.videoThumbnails) {
-            if (d.quality === quality) {
-                return d.url;
+    async getThumbnail(media_id, quality = "maxres"){
+        media_id = urlParse(media_id);
+        const data = await this.getMediaMetadata(media_id);
+        for (const q of data["videoThumbnails"]){
+            if (q["quality"] == quality){
+                return q["url"];
             }
         }
     }
 
-    /*
-    *   @param {string} video_id
-    *   @param {number} audio_or_video
-    *   @param {string} quality
-    *   @return {string}
-    */
-    async save(id, audio_or_video, quality) {
-        let data = await this.download(id, audio_or_video, quality);
-        if (data == null) throw "Error while downloading";
-        const bffer = data.data;
-        const name = data.title.replace(/\//g, "-");
-        if (data) {
-            const ext = audio_or_video == 1 ? ".mp3" : ".mp4"
-            fs.writeFileSync(name + ext, bffer);
-            this.cli_log("Success!");
-            return name + ext;
+    async processDownloadBody(media_id, media_type, quality) {
+        if (media_type === "subs") {
+            throw new Error("Subs are not implemented yet!");
         }
-        throw "Error while saving";
+        if (!["mixed", "video", "audio", "subs"].includes(media_type)) {
+            throw new TypeError("Media type can only be 'mixed', 'video', 'audio' or 'subs'");
+        }
+        const data = await this.getMediaInfo(media_id);
+        const available_qualities_infos = data[media_type];
+        if (!quality && media_type !== "subs") {
+            quality = Math.max(...available_qualities_infos.map(x => x.quality));
+        }
+        const item = available_qualities_infos.find(x => x.quality === quality);
+        if (item) {
+            return {
+                id: media_id,
+                title: data.title,
+                query: item.query,
+                extension: item.extension,
+            };
+        }
+        console.error("Cannot process download body! Please, check the parameters");
+        throw new Error("Cannot process download body! Please, check the parameters");
     }
 
-    /*
-    *   @param {string} playlist_id
-    *   @return {Array}
-    *   @description: Get all the videos from the playlist
-    */
-    async playlist(playlist_id) {
-        const request_url = this.base_api_url + "playlists/" + playlist_id;
-        const data = await fetcher(request_url);
-        const videos = data.videos;
-        return videos;
+    async download(media_id, media_type="mixed", quality=null) {
+        const body = await this.processDownloadBody(media_id, media_type, quality);
+        const headers ={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0",
+            "Accept": "*/*",
+            "DNT": 1,
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": 1,
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Sec-GPC": 1,
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+            "Content-type": "application/x-www-form-urlencoded"
+        }
+        const body_query = `id=${body.id}&title=${body.title}&download_widget=${encodeURIComponent(body.query)}`;
+        const response = await post(this.download_endpoint, body_query, headers, "arraybuffer");
+        const result = {
+            "title": body["title"],
+            "type": media_type,
+            "extension": body["extension"],
+            "data": response,
+        };
+        return result;
+    }
+
+    async save(media_id, media_type = "mixed", quality = null) {
+        const response = await this.download(media_id, media_type, quality);
+        let extension = response.extension;
+        if (media_type === "audio") {
+          extension = "mp3";
+        }
+        const filename = `${response.title.replace("/", "-").replace("|", "")}.${extension}`;
+        await fs.writeFile(filename, response.data);
+        return response.title;
     }
 }
 
 
-export default KamTube;
+export {
+    KamTube
+};
